2f1d3842a02687ce9cf4d033ff454ab6acabc014
manpreetkaur <Kaur4285@sheridancollege.ca>
2025-03-01 21:33:37 -0500

    Initial Commit

---
+++ Group6_SE/Group6_SEApp.swift
@@ -0,0 +1,17 @@
+//
+//  Group6_SEApp.swift
+//  Group6_SE
+//
+//  Created by manpreet kaur on 2025-03-01.
+//
+
+import SwiftUI
+
+@main
+struct Group6_SEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
